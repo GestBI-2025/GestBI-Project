5b3643706596df6af6fc4a0838ac133b1920f641
EpiCoder_# <gestbiregistocivil@gmail.com>
2026-01-15 14:34:46 +0100

    Gest_BI
    editar Marcacao - Modal

--- pages/admin-page/JS/scriptCode.js
+++ pages/admin-page/JS/scriptCode.js
@@ -17,3 +17,35 @@ const editarBtn = document.querySelectorAll('.editar');
 editarBtn.onclick =  ()=>{
     console.log('ok');
 }
+
+
+// Fechar o menu quando clicar fora.
+// document.addEventListener('click', (e) => {
+//     const target = e.target;
+//     if (!modalEditar.classList.contains('show')) return;
+//     if (modalEditar.contains(target) || modalEditar.contains(target)) return;
+//     modalEditar.classList.remove('show')
+// })
+
+
+// Fechar o menu quando clicar no Esc (Escape)
+
+// document.addEventListener('keydown', (letra) => {
+//     if (letra.key === 'Escape') {
+//         if (modalEditar.classList.contains('show')) {
+//             modalEditar.classList.remove('show');
+//         }
+
+//     }
+// })
+
+// ------ MODAL-------
+function fecharModal() {
+  modalEditar.classList.remove("show");
+  modalEditar.setAttribute("aria-hidden", "true");
+}
+
+function abrirModal() {
+  modalEditar.classList.add("show");
+  modalEditar.setAttribute("aria-hidden", "false");
+}
